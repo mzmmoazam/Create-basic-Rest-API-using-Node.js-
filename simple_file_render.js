9b81ea958647f6b57e2a6aa75ad70bcfca941df2
Moazam <mzm.moazam@gmail.com>
2017-09-24 10:09:20 +0530

    Create simple_file_render.js

---
+++ simple_file_render.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var app     = express();
+app.use(express.static(__dirname + '/View'));
+//Store all HTML files in view folder.
+app.use(express.static(__dirname + '/Script'));
+//Store all JS and CSS in Scripts folder.
+
+app.get('/',function(req,res){
+  res.sendFile('index.html');
+  //It will find and locate index.html from View or Scripts
+});
+
+app.get('/about',function(req,res){
+  res.sendFile('/about.html');
+});
+
+app.get('/sitemap',function(req,res){
+  res.sendFile('/sitemap.html');
+});
+
+app.listen(8088);
+
+console.log("Running at Port 8088");
